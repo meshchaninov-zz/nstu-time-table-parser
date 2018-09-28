8b6662f80ed339ad0fbf5f5f0fa94ba3bd28b015
Nikita Meshchaninov <nikita@MacBook-Pro-Nikita.local>
2018-09-28 23:15:41 +0700

    Парсится head

--- parser.py
+++ parser.py
@@ -1,8 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
+URL = "https://ciu.nstu.ru/student/time_table_view?idgroup=25554&fk_timetable=36218&nomenu=1&print=1"
+
+def get_html(URL):
+    return requests.get(URL).text
+
+def parse_head(html):
+    soup = BeautifulSoup(html, "lxml")
+    soup_head = soup.find("table").find("table")
+    today = soup_head.find("em").text.split()
+    group_and_sem = soup_head.find(lambda tag: len(tag.attrs) == 2).text.split()
+    group = group_and_sem[1]
+    sem = group_and_sem[2]
+    print(group, sem)
+    
+
+
 def main():
-    pass
+    html = get_html(URL)
+    parse_head(html)
 
 if __name__ == '__main__':
     main()
