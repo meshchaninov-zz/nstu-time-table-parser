c7a2a2c587941c504e361240451175608e88c64f
Nikita Meshchaninov <nikita@MacBook-Pro-Nikita.local>
2018-09-28 23:34:45 +0700

    доавил парсинг "сегодня"

--- parser.py
+++ parser.py
@@ -6,16 +6,18 @@ URL = "https://ciu.nstu.ru/student/time_table_view?idgroup=25554&fk_timetable=36
 def get_html(URL):
     return requests.get(URL).text
 
+def parse_today_from_parse_head(soup):
+    MONTHS = ("января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря")
+    t_a_w = soup.find("em").text.split()
+    return (int(t_a_w[1]), MONTHS.index(t_a_w[2]) + 1, int(t_a_w[3][:-1]), int(t_a_w[6]))
+
 def parse_head(html):
     soup = BeautifulSoup(html, "lxml")
     soup_head = soup.find("table").find("table")
-    today = soup_head.find("em").text.split()
     group_and_sem = soup_head.find(lambda tag: len(tag.attrs) == 2).text.split()
     group = group_and_sem[1]
     sem = group_and_sem[2]
-    print(group, sem)
     
-
 
 def main():
     html = get_html(URL)
