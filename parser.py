d5dc7d24ed72efdbf85d3691cab32a509c98d849
Nikita Meshchaninov <nikita@MacBook-Pro-Nikita.local>
2018-09-30 01:31:42 +0700

    Рефакторинг, перевел docs разбил функции, прокоментил страшный код

--- parser.py
+++ parser.py
@@ -9,15 +9,20 @@ def get_html(URL):
     return requests.get(URL).text
 
 def parse_today_from_parse_head(soup):
-    """This func return today date and week from head soup"""
+    """
+    Вспопмогательная функция
+    Принимает на вход soup из bs4, возвращает полный вид даты
+    """
     MONTHS = ("января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря")
     t_a_w = soup.find("em").text.split()
     return (int(t_a_w[1]), MONTHS.index(t_a_w[2]) + 1, int(t_a_w[3][:-1])), int(t_a_w[6])
 
 
-def parse_head(html):
-    """This func return (today date, week, group, semestr)"""
-    soup = BeautifulSoup(html, "lxml")
+def parse_head(soup):
+    """
+    Принимает soup из bs4
+    Возвращает (полный вид дня, номер недели, группа, семестр)
+    """
     soup_head = soup.find("table").find("table")
     today = parse_today_from_parse_head(soup_head)
     group_and_sem = soup_head.find(lambda tag: len(tag.attrs) == 2).text.split()
@@ -25,40 +30,53 @@ def parse_head(html):
     sem = int(group_and_sem[2])
     return today[0], today[1], group, sem
 
-#TODO: next level!
-def parse_body(html):
-    soup = BeautifulSoup(html, "lxml")
-    soup_body = soup.findAll("td")[10:]
-    table = [re.sub(r"^\s+|\n|\xa0|$\s+",'', i.text) for i in soup_body]
-    WEEK = ("Понедельник", "Вторник","Среда", "Четверг", "Пятница", "Суббота", "Воскресенье")
+def structed_output_from_parse_body(table):
+    """
+    Вспомогательная функция
+    Принимает на вход кашу из parse body и приводит её в человеческий вид
+    """
+    WEEK = ("Вторник","Среда", "Четверг", "Пятница", "Суббота")
     EVEN_AND_NOT_EVEN = ("Ч", "Н")
     result = []
     day = []
     line = []
-    num = 0
+    num = -1
     for text in table:
         text = text.strip()
-        if text in WEEK:
+        if text in WEEK: #Если встретилось слово из недели, то достиг конец предедущего дня (исключение - Понедельник)
             result.append(day)
             day = []
             day.append(text)
             continue
-        if line == [] and text in EVEN_AND_NOT_EVEN:
+        if line == [] and text in EVEN_AND_NOT_EVEN:   #Если встретилась Ч или Н, но без времени, то вот
             line.append(day[-1][0])
             num+=1
         line.append(text)
         num+=1
-        if num >= 4:
+        if num >= 4: #Четыре подряд элемента образуют строку в исходной таблице
             day.append(line)
             line = []
             num = 0
     result.append(day)
-    result.pop(0)
+    result[0].insert(0,result[0][0].pop(0)) #Перестановка слова "Понедельник" за пределы вложеного списка (Фуух, оптимизация епт)
     return result
+
+
+def parse_body(soup):
+    """
+    Принимает soup из bs4
+    Возвращает само расписание в виде [[день недели [время, Ч/Н, предмет, кабинет],[...],[...]]...]
+    """
+    soup_body = soup.findAll("td")[10:]
+    table = [re.sub(r"^\s+|\n|\xa0|$\s+",'', i.text) for i in soup_body]
+    print(table)
+    return structed_output_from_parse_body(table)
 
 def main():
     html = get_html(URL)
-    print(parse_body(html))
+    soup = BeautifulSoup(html, "lxml")
+    parse_head(soup)
+    parse_body(soup)
 
 if __name__ == '__main__':
     main()
